055993efd0726d01d75917fa82976ed771715cdc
Lakshitha <hrldias@gmail.com>
2025-11-04 23:37:30 +0530

    Innitial commit 2025.11.04

---
+++ js/custom.js
@@ -0,0 +1,38 @@
+/**
+ * @file
+ * Global utilities.
+ *
+ */
+(function($, Drupal) {
+
+  'use strict';
+
+  Drupal.behaviors.bootstrap_sass = {
+    attach: function(context, settings) {
+
+      // Custom code here
+     
+     
+
+    }
+  };
+
+})(jQuery, Drupal);
+
+(function ($) {
+  $(document).ready(function () {
+    // Only apply hover on desktop
+    if (window.matchMedia("(min-width: 992px)").matches) {
+      $('.dropdown').hover(
+        function () {
+          $(this).addClass('show');
+          $(this).find('.dropdown-menu').addClass('show');
+        },
+        function () {
+          $(this).removeClass('show');
+          $(this).find('.dropdown-menu').removeClass('show');
+        }
+      );
+    }
+  });
+})(jQuery);
