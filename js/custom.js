2f06bda2abd25613397d101bb884acce899df7fd
Lakshitha <hrldias@gmail.com>
2025-11-23 11:34:33 +0530

    Some refinments

--- js/custom.js
+++ js/custom.js
@@ -11,8 +11,7 @@
     attach: function(context, settings) {
 
       // Custom code here
-     
-     
+      
 
     }
   };
@@ -36,3 +35,18 @@
     }
   });
 })(jQuery);
+
+(function (Drupal, $, once) {
+  Drupal.behaviors.loginModalTrigger = {
+    attach: function (context) {
+      $(once('loginModalTrigger', '.js-open-login-popup', context))
+        .on('click', function (e) {
+          e.preventDefault();
+          console.log('Button clicked, triggering modal');
+
+          const modal = new bootstrap.Modal(document.getElementById('loginModal'));
+          modal.show();
+        });
+    }
+  };
+})(Drupal, jQuery, once);
